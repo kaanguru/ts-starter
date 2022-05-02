45371bb5e63ddef007ff2aa4f712fd122488402d
CEM KAAN KÖSALI <cemkaan@inbox.ru>
2022-05-02 11:23:38 +0300

    ts-starter

---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var fs = require("fs");
+// list of all the files in folder
+var files = fs.readdirSync("./src");
+console.log("files :>> ", files[1]);
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import * as fs from "fs";
+
+// list of all the files in folder
+const files = fs.readdirSync("./src");
+console.log("files :>> ", files[1]);
