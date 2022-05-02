757937cc70df6058f696ce600bb5ba2c7bd2fa37
CEM KAAN KÖSALI <cemkaan@inbox.ru>
2022-05-02 11:40:29 +0300

    create 1 file and update 3 files

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ exports.__esModule = true;
 var fs = require("fs");
 // list of all the files in folder
 var files = fs.readdirSync("./src");
-console.log("files :>> ", files[1]);
+console.log("files :>> ", files[0]);
--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import * as fs from "fs";
 
 // list of all the files in folder
 const files = fs.readdirSync("./src");
-console.log("files :>> ", files[1]);
+console.log("files :>> ", files[0]);
